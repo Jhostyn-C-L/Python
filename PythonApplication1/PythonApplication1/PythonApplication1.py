878d40f74bfa93b2754a9726f87cfeece32bf3d6
Jhostyn-C-L <jhostynmanuelcoatslerebours@gmail.com>
2026-07-15 17:14:29 -0400

    send

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,27 @@
+﻿def romanoadecimal(Parametro):
+    romano = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    resultado = 0
+    indice = 0
+    
+    while indice < len(Parametro):
+        
+        if indice + 1 < len(Parametro) and romano[Parametro[indice]] < romano[Parametro[indice + 1]]:
+            resultado += romano[Parametro[indice + 1]] - romano[Parametro[indice]]
+            indice += 2 
+        else:
+            resultado += romano[Parametro[indice]]
+            indice += 1 
+    return resultado
+
+Inicio = input("Introduzca el numero romano para convertir a decimal: ").upper()
+total = romanoadecimal(Inicio)
+print("Convertido a decimal: ", total)
