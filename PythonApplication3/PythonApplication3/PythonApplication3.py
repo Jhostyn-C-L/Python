71482040ece0c028027bba61a35985fd3ff67272
Jhostyn-C-L <jhostynmanuelcoatslerebours@gmail.com>
2026-07-16 18:38:00 -0400

    send

---
+++ PythonApplication3/PythonApplication3/PythonApplication3.py
@@ -0,0 +1,15 @@
+﻿def insertionSort(A):
+    i = 1
+    while i < len(A):
+        j = i
+        while j > 0 and A[j-1] > A[j]:
+            A[j], A[j-1] = A[j-1], A[j]
+            j = j - 1
+        i = i + 1
+    return A
+
+Entrada = input("Introduzca los números para ordenarlos de menor a mayor(recuerde separarlos por espacios los números): ")
+Listado = [int(x) for x in Entrada.split()]
+
+print("Listado original: ", Listado)
+print("Listado organizado: ", insertionSort(Listado))
